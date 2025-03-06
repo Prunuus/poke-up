73031740b5f3fa7511ebdeab18d0722f0e25c390
Varish Kongara <varish.kongara@gmail.com>
2025-03-05 22:08:17 -0600

    created a different tsconfig for run dev and run build

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,13 +1,13 @@
-import express, { Request, Response } from "express";
+import express from "express";
 
 const router = express.Router();
 
 // Define routes inside the controller file
-router.get("/", (req: Request, res: Response) => {
+router.get("/", (res: express.Response) => {
   res.json({ message: "Get all users" });
 });
 
-router.get("/:id", (req: Request, res: Response) => {
+router.get("/:id", (req: express.Request, res: express.Response) => {
   res.json({ message: `Get user with ID: ${req.params.id}` });
 });
 
