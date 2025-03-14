2fbd94e5d5e37a56116d2e64bb9111c2d6eaa6d5
Varish Kongara <varish.kongara@gmail.com>
2025-03-14 01:20:47 -0500

    added user pokemon schema

--- backend/src/schemas/userSchema.ts
+++ backend/src/schemas/userSchema.ts
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 // import Task from "./taskSchema.ts";
 import taskSchema from "./taskSchema.ts";
+import userMonSchema from "./userMon.ts";
 
 const userSchema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
-  Pokemon: Array,
-  Team: Array,
+  Pokemon: [userMonSchema],
+  Team: [userMonSchema],
   Exp: Number,
   Tasks: [taskSchema],
 });
