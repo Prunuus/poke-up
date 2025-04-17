1d39d0076dde5a2d80aa80ed7411ad1892511acc
Lorenzo Viray <103648812+CrunchyWaterIsNotIce@users.noreply.github.com>
2025-04-17 17:27:10 -0500

    FRONTEND GODS HELP ME (#34)
    Co-authored-by: Lorenzo Viray <lviray2006@gmail.com>
    Co-authored-by: kevin chen <167628964+Prunuus@users.noreply.github.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './styles/global.css';
 import LandingPage from './pages/landing-page/landing-page';
+
+import SignIn from './pages/sign-in/sign-in';
+import SignUp from './pages/sign-up/sign-up';
+
 import Study from './pages/study/study';
+
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
+
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+
           <Route path="/" element={<LandingPage/>} />
           <Route path="/study" element={<Study/>} />
+
         </Routes>
       </Router>
 
--- frontend/src/pages/sign-in/sign-in.tsx
+++ frontend/src/pages/sign-in/sign-in.tsx
@@ -6,8 +6,50 @@ function SignIn() {
     <>
       <div className="background-wrapper">
 
+        {/* Static Background Elements */}
 
-      </div>
+        {/* <img src="../assets/SignLoginFrame.svg" className ="frame"/> */}
+        <img src="../../assets/SignLoginBackground.svg" className="background"/>
+        <img src="../../assets/SignLoginBanner.svg" className="banner"/>
+
+        {/* Navigation Elements */}
+        <div className="nav-bar-login">
+          <div style={ {display: "flex", gap: "7vw"} }>
+            <button className="nav-button">HOME</button>
+            <button className="nav-button">ABOUT</button>
+          </div>
+          <div style={{ display: "flex", gap: "4vw"}}>
+            <button className="nav-button">CREDITS</button>
+          </div>
+
+          <div className="login-container">
+            <div className="welcome-header">Welcome back,</div>
+            <div className="welcome-header">Trainer!</div>
+            <div className="login-frame">
+              <input type="text" placeholder="Username" className="input-box" />
+              <input type="password" placeholder="Password" className="input-box" />
+              <p className="register-text">Don't have an account? <a href="your-registration-page-url" className="register-link">Register here</a></p>
+              <button className="sign-in-btn">Sign In</button>
+            </div>
+          </div>
+
+
+
+        </div>
+        <div>
+
+        </div>
+
+
+        {/* Footer Elements */}
+        <div className="foot-bar">
+          <img src="../assets/Instagram.svg" className="foot-button"></img>
+          <img src="../assets/Facebook.svg" className="foot-button"></img>
+          <img src="../assets/Youtube.svg" className="foot-button"></img>
+        </div>
+
+
+        </div>
     
     </>
 
